29f4e8f51331cf8f3679991be0399e858dfc5af4
akimegr <akim.egor2013@yandex.ru>
2022-11-13 17:10:22 +0300

    git: add ui balance

--- src/main/java/com/shop/prices/services/servicesImpl/PriceRenderImpl.java
+++ src/main/java/com/shop/prices/services/servicesImpl/PriceRenderImpl.java
@@ -1,10 +1,10 @@
-package com.shop.prices.services.servicesImpl;
-
-import com.shop.prices.services.servicesImpl.PriceRender;
-
-public class PriceRenderImpl implements PriceRender {
-    @Override
-    public double takePrice(String price) {
-        return Double.parseDouble(price);
-    }
-}
+//package com.shop.prices.services.servicesImpl;
+//
+//import com.shop.prices.services.servicesImpl.PriceRender;
+//
+//public class PriceRenderImpl implements PriceRender {
+//    @Override
+//    public double takePrice(String price) {
+//        return Double.parseDouble(price);
+//    }
+//}
